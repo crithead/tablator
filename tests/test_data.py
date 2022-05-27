8f7711b00c4f9e3b29850d6d2aa4c69a13e956d8
Generic Person <gene@crithead.com>
2022-05-27 09:49:02 -0700

    Add tests for data.is_table

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,19 +1,43 @@
 # Unit tests for tablator.data functions.
 
-import tablator
+import os
+import pytest
+import tablator.data as data
 
-def test_is_table_1():
-    #tablator.data.is_table()
-    assert True
+def test_is_table_yes(monkeypatch):
+    def mock_os_listdir(dir_name):
+        return ['shiny-things.yaml', 'sparkly-things.json']
+
+    monkeypatch.setattr(os, "listdir", mock_os_listdir)
+    assert data.is_table('shiny-things') is True
+    assert data.is_table('sparkly-things') is True
+
+
+def test_is_table_no(monkeypatch):
+    def mock_os_listdir(dir_name):
+        return ['shiny-things.yaml', 'sparkly-things.json']
+
+    monkeypatch.setattr(os, "listdir", mock_os_listdir)
+    assert data.is_table('shady-things') is False
+
+
+def test_is_table_no_table_name():
+    with pytest.raises(ValueError, match=r'table_name is None'):
+        data.is_table()
+    with pytest.raises(ValueError, match=r'table_name is None'):
+        data.is_table(None)
+
 
 def test_list_tables_1():
-    # tablator.data.list_tables()
+    # data.list_tables()
     assert True
+
 
 def test_load_1():
-    # tablator.data.load()
+    # data.load()
     assert True
 
+
 def test_set_data_dir_1():
-    # tablator.data.set_data_dir()
+    # data.set_data_dir()
     assert True
